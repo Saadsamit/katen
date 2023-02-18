551346b07aff499f1b8f3ea8ee4d962719051a32
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-02-19 02:18:56 +0600

    nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-
+let nav = document.getElementById('side_nav').classList
+document.getElementById('nav_btn').addEventListener('click',function(){
+    nav.add('active');
+ });
+document.getElementById('cancel_btn').addEventListener('click',function(){
+   nav.remove('active');
+});
