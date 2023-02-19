b0a6f1ec7b8d7941618a6383cc6041037840727e
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-02-19 16:55:07 +0600

    nav

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ document.getElementById('nav_btn').addEventListener('click',function(){
 document.getElementById('cancel_btn').addEventListener('click',function(){
    nav.remove('active');
 });
+let Search = document.getElementById('Search').classList
+document.getElementById('search_btn').addEventListener('click',function(){
+   Search.add('active');
+});
+document.getElementById('search_cancel_btn').addEventListener('click',function(){
+   Search.remove('active');
+});
